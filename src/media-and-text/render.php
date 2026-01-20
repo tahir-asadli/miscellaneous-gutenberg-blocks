83af31517748f14cf20feb839f43c094bf51a9e4
Tahir Asadli <tahir-asadov@outlook.com>
2026-01-21 01:41:39 +0400

    Fix coding style

--- src/media-and-text/render.php
+++ src/media-and-text/render.php
@@ -1,96 +1,111 @@
 <?php
-if (!defined('ABSPATH')) {
-  exit; // Exit if accessed directly
-}
-$block_booster_reversed = !empty($attributes['reversed']) && $attributes['reversed'] == 1;
-$block_booster_tabletReversed = !empty($attributes['mgb_tabletReversed']) && $attributes['mgb_tabletReversed'] == 1;
-$block_booster_mobileReversed = !empty($attributes['mobileReversed']) && $attributes['mobileReversed'] == 1;
-$block_booster_stacked = !empty($attributes['stacked']) && $attributes['stacked'] == 1;
-$block_booster_tabletStacked = !empty($attributes['tabletStacked']) && $attributes['tabletStacked'] == 1;
-$block_booster_mobileStacked = !empty($attributes['mobileStacked']) && $attributes['mobileStacked'] == 1;
-$block_booster_gap = !empty($attributes['gap']) ? $attributes['gap'] : 0;
-$block_booster_tabletGap = !empty($attributes['tabletGap']) ? $attributes['tabletGap'] : 0;
-$block_booster_mobileGap = !empty($attributes['mobileGap']) ? $attributes['mobileGap'] : 0;
-$block_booster_hasSingleChild = count($block->inner_blocks) == 1;
-$block_booster_classes = [];
-if ($block_booster_reversed) {
-  $block_booster_classes[] = 'block-booster-media-and-text--is-reversed';
-}
-if ($block_booster_tabletReversed) {
-  $block_booster_classes[] = 'block-booster-media-and-text--tablet-is-reversed';
-}
-if ($block_booster_mobileReversed) {
-  $block_booster_classes[] = 'block-booster-media-and-text--mobile-is-reversed';
+/**
+ * Media & Text
+ *
+ * @package block-booster
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
 
-if ($block_booster_stacked) {
-  $block_booster_classes[] = 'block-booster-media-and-text--is-stacked';
+$block_booster_reversed         = ! empty( $attributes['reversed'] ) && 1 === $attributes['reversed'];
+$block_booster_tablet_reversed  = ! empty( $attributes['tabletReversed'] ) && 1 === $attributes['tabletReversed'];
+$block_booster_mobile_reversed  = ! empty( $attributes['mobileReversed'] ) && 1 === $attributes['mobileReversed'];
+$block_booster_stacked          = ! empty( $attributes['stacked'] ) && 1 === $attributes['stacked'];
+$block_booster_tablet_stacked   = ! empty( $attributes['tabletStacked'] ) && 1 === $attributes['tabletStacked'];
+$block_booster_mobile_stacked   = ! empty( $attributes['mobileStacked'] ) && 1 === $attributes['mobileStacked'];
+$block_booster_gap              = ! empty( $attributes['gap'] ) ? $attributes['gap'] : 0;
+$block_booster_tablet_gap       = ! empty( $attributes['tabletGap'] ) ? $attributes['tabletGap'] : 0;
+$block_booster_mobile_gap       = ! empty( $attributes['mobileGap'] ) ? $attributes['mobileGap'] : 0;
+$block_booster_has_single_child = count( $block->inner_blocks ) == 1;
+$block_booster_classes          = array();
+if ( $block_booster_reversed ) {
+	$block_booster_classes[] = 'block-booster-media-and-text--is-reversed';
 }
-if ($block_booster_tabletStacked) {
-  $block_booster_classes[] = 'block-booster-media-and-text--tablet-is-stacked';
+if ( $block_booster_tablet_reversed ) {
+	$block_booster_classes[] = 'block-booster-media-and-text--tablet-is-reversed';
 }
-if ($block_booster_mobileStacked) {
-  $block_booster_classes[] = 'block-booster-media-and-text--mobile-is-stacked';
+if ( $block_booster_mobile_reversed ) {
+	$block_booster_classes[] = 'block-booster-media-and-text--mobile-is-reversed';
+}
+
+if ( $block_booster_stacked ) {
+	$block_booster_classes[] = 'block-booster-media-and-text--is-stacked';
+}
+if ( $block_booster_tablet_stacked ) {
+	$block_booster_classes[] = 'block-booster-media-and-text--tablet-is-stacked';
+}
+if ( $block_booster_mobile_stacked ) {
+	$block_booster_classes[] = 'block-booster-media-and-text--mobile-is-stacked';
 }
 
 
-if ($block_booster_hasSingleChild) {
-  $block_booster_classes[] = 'block-booster-media-and-text--single-child';
+if ( $block_booster_has_single_child ) {
+	$block_booster_classes[] = 'block-booster-media-and-text--single-child';
 }
-$block_booster_additional_attributes['class'] = join(' ', $block_booster_classes);
-$block_booster_additional_attributes['id'] = 'block-booster-' . uniqid();
+$block_booster_additional_attributes['class'] = join( ' ', $block_booster_classes );
+$block_booster_additional_attributes['id']    = 'block-booster-' . uniqid();
 
 // Prepare wrapper attributes safely.
-$block_booster_wrapper_attrs = get_block_wrapper_attributes($block_booster_additional_attributes);
+$block_booster_wrapper_attrs = get_block_wrapper_attributes( $block_booster_additional_attributes );
 ?>
+<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 <div <?php echo $block_booster_wrapper_attrs; ?>>
-  <div class="block-booster-media-and-text--left">
-    <?php if (!empty($attributes['imageUrl'])) { ?>
-      <img src="<?php echo esc_url($attributes['imageUrl']); ?>" alt="<?php echo esc_attr($attributes['imageName']); ?>">
-    <?php } ?>
-  </div>
-  <div class="block-booster-media-and-text--right">
-    <?php
+	<div class="block-booster-media-and-text--left">
+	<?php if ( ! empty( $attributes['imageUrl'] ) ) { ?>
+		<img src="<?php echo esc_url( $attributes['imageUrl'] ); ?>" alt="<?php echo esc_attr( $attributes['imageName'] ); ?>">
+	<?php } ?>
+	</div>
+	<div class="block-booster-media-and-text--right">
+	<?php
 
-    if (!empty($block->inner_blocks)) {
-      foreach ($block->inner_blocks as $block_booster_inner_block) {
-        echo '<div>' . wp_kses_post($block_booster_inner_block->render()) . '</div>';
-      }
-    }
-    ?> ?>
-  </div>
+	if ( ! empty( $block->inner_blocks ) ) {
+		foreach ( $block->inner_blocks as $block_booster_inner_block ) {
+			echo '<div>' . wp_kses_post( $block_booster_inner_block->render() ) . '</div>';
+		}
+	}
+	?>
+	?>
+	</div>
 </div>
 <style>
-  <?php echo esc_html(BLOCK_BOOSTER_MIN_DESKTOP_BREAKING_POINT); ?>
-  ;
+	<?php echo esc_html( BLOCK_BOOSTER_MIN_DESKTOP_BREAKING_POINT ); ?>
+	;
 
-  ?>#<?php echo esc_attr($block_booster_additional_attributes['id']); ?> {
-    <?php $gap_desktop = (int) $block_booster_gap;
-    echo esc_attr("gap: {$gap_desktop}px;"); ?>
-    <?php echo esc_html("gap: {$block_booster_gap}px;"); ?>
-  }
-  }
+	?>#<?php echo esc_attr( $block_booster_additional_attributes['id'] ); ?> {
+	<?php
+	$gap_desktop = (int) $block_booster_gap;
+	echo esc_attr( "gap: {$gap_desktop}px;" );
+	?>
+	<?php echo esc_html( "gap: {$block_booster_gap}px;" ); ?>
+	}
+	}
 
-  <?php echo esc_html(BLOCK_BOOSTER_MIN_TABLET_BREAKING_POINT); ?>
-  ;
-  ?>
-  <?php echo esc_html(BLOCK_BOOSTER_MAX_TABLET_BREAKING_POINT); ?>
-  ;
+	<?php echo esc_html( BLOCK_BOOSTER_MIN_TABLET_BREAKING_POINT ); ?>
+	;
+	?>
+	<?php echo esc_html( BLOCK_BOOSTER_MAX_TABLET_BREAKING_POINT ); ?>
+	;
 
-  ?>#<?php echo esc_attr($block_booster_additional_attributes['id']); ?> {
-    <?php $gap_tablet = (int) $block_booster_tabletGap;
-    echo esc_attr("gap: {$gap_tablet}px;"); ?>
-    <?php echo esc_html("gap: {$block_booster_tabletGap}px;"); ?>
-  }
-  }
+	?>#<?php echo esc_attr( $block_booster_additional_attributes['id'] ); ?> {
+	<?php
+	$gap_tablet = (int) $block_booster_tablet_gap;
+	echo esc_attr( "gap: {$gap_tablet}px;" );
+	?>
+	<?php echo esc_html( "gap: {$block_booster_tablet_gap}px;" ); ?>
+	}
+	}
 
-  <?php echo esc_html(BLOCK_BOOSTER_MAX_MOBILE_BREAKING_POINT); ?>
-  ;
+	<?php echo esc_html( BLOCK_BOOSTER_MAX_MOBILE_BREAKING_POINT ); ?>
+	;
 
-  ?>#<?php echo esc_attr($block_booster_additional_attributes['id']); ?> {
-    <?php $gap_mobile = (int) $block_booster_mobileGap;
-    echo esc_attr("gap: {$gap_mobile}px;"); ?>
-    <?php echo esc_html("gap: {$block_booster_mobileGap}px;"); ?>
-  }
-  }
+	?>#<?php echo esc_attr( $block_booster_additional_attributes['id'] ); ?> {
+	<?php
+	$gap_mobile = (int) $block_booster_mobile_gap;
+	echo esc_attr( "gap: {$gap_mobile}px;" );
+	?>
+	<?php echo esc_html( "gap: {$block_booster_mobile_gap}px;" ); ?>
+	}
+	}
 </style>
--- src/search/render.php
+++ src/search/render.php
@@ -1,55 +1,63 @@
 <?php
-if (!defined('ABSPATH')) {
-  exit; // Exit if accessed directly
-}
-$block_booster_showCategory = !empty($attributes['showCategory']) && $attributes['showCategory'] == 1;
-$block_booster_showSearchIcon = !empty($attributes['showSearchIcon']) && $attributes['showSearchIcon'] == 1;
-$block_booster_disableCSS = !empty($attributes['disableCSS']) && $attributes['disableCSS'] == 1;
-$block_booster_searchPlaceholder = !empty($attributes['searchPlaceholder']) ? $attributes['searchPlaceholder'] : '';
-$block_booster_categoryText = !empty($attributes['categoryText']) ? $attributes['categoryText'] : '';
-$block_booster_buttonText = !empty($attributes['buttonText']) ? $attributes['buttonText'] : '';
-$block_booster_height = !empty($attributes['height']) && (int) $attributes['height'] > 0 ? (int) $attributes['height'] : '0';
-$block_booster_width = !empty($attributes['width']) && (int) $attributes['width'] > 0 ? (int) $attributes['width'] : '0';
+/**
+ * Search
+ *
+ * @package block-booster
+ */
 
-$block_booster_classes = [];
-if (!$block_booster_disableCSS) {
-  $block_booster_classes[] = 'has-style';
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
-if ($block_booster_showSearchIcon) {
-  $block_booster_classes[] = 'show-search-icon';
+
+$block_booster_show_category      = ! empty( $attributes['showCategory'] ) && 1 === $attributes['showCategory'];
+$block_booster_show_search_icon   = ! empty( $attributes['showSearchIcon'] ) && 1 === $attributes['showSearchIcon'];
+$block_booster_disable_css        = ! empty( $attributes['disableCSS'] ) && 1 === $attributes['disableCSS'];
+$block_booster_search_placeholder = ! empty( $attributes['searchPlaceholder'] ) ? $attributes['searchPlaceholder'] : '';
+$block_booster_category_text      = ! empty( $attributes['categoryText'] ) ? $attributes['categoryText'] : '';
+$block_booster_button_text        = ! empty( $attributes['buttonText'] ) ? $attributes['buttonText'] : '';
+$block_booster_height             = ! empty( $attributes['height'] ) && (int) $attributes['height'] > 0 ? (int) $attributes['height'] : '0';
+$block_booster_width              = ! empty( $attributes['width'] ) && (int) $attributes['width'] > 0 ? (int) $attributes['width'] : '0';
+
+$block_booster_classes = array();
+if ( ! $block_booster_disable_css ) {
+	$block_booster_classes[] = 'has-style';
 }
-if ($block_booster_showCategory) {
-  $block_booster_classes[] = 'show-category';
+if ( $block_booster_show_search_icon ) {
+	$block_booster_classes[] = 'show-search-icon';
 }
-$block_booster_additional_attributes['class'] = join(' ', $block_booster_classes);
-$block_booster_additional_attributes['id'] = 'block-booster-' . uniqid();
-$block_booster_categories = get_categories();
+if ( $block_booster_show_category ) {
+	$block_booster_classes[] = 'show-category';
+}
+$block_booster_additional_attributes['class'] = join( ' ', $block_booster_classes );
+$block_booster_additional_attributes['id']    = 'block-booster-' . uniqid();
+$block_booster_categories                     = get_categories();
 ?>
 <div class="block-booster-search-container">
-  <form action="<?php echo esc_url(home_url('/')); ?>">
-    <div <?php echo get_block_wrapper_attributes($block_booster_additional_attributes); ?>>
-      <?php if ($block_booster_showCategory && !empty($block_booster_categories)) { ?>
-        <select name="cat" class="search-category">
-          <?php if ($block_booster_categoryText != '') { ?>
-            <option value=""><?php echo esc_html($block_booster_categoryText); ?></option>
-          <?php } ?>
-          <?php foreach ($block_booster_categories as $block_booster_category) { ?>
-            <option <?php selected(get_query_var('cat'), $block_booster_category->term_id); ?> value="<?php echo esc_attr($block_booster_category->term_id); ?>"><?php echo esc_html($block_booster_category->name); ?></option>
-          <?php } ?>
-        </select>
-      <?php } ?>
-      <input name="s" type="search" value="<?php echo esc_attr(get_query_var('s')); ?>" class="search-input" placeholder="<?php echo esc_attr($block_booster_searchPlaceholder); ?>" />
-      <button type="submit" class="search-button"><?php echo !$block_booster_showSearchIcon && $block_booster_buttonText ? esc_html($block_booster_buttonText) : '&nbsp;' ?></button>
-    </div>
-  </form>
+	<form action="<?php echo esc_url( home_url( '/' ) ); ?>">
+  <?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+	<div <?php echo get_block_wrapper_attributes( $block_booster_additional_attributes ); ?>>
+		<?php if ( $block_booster_show_category && ! empty( $block_booster_categories ) ) { ?>
+		<select name="cat" class="search-category">
+			<?php if ( '' !== $block_booster_category_text ) { ?>
+			<option value=""><?php echo esc_html( $block_booster_category_text ); ?></option>
+			<?php } ?>
+			<?php foreach ( $block_booster_categories as $block_booster_category ) { ?>
+			<option <?php selected( get_query_var( 'cat' ), $block_booster_category->term_id ); ?> value="<?php echo esc_attr( $block_booster_category->term_id ); ?>"><?php echo esc_html( $block_booster_category->name ); ?></option>
+			<?php } ?>
+		</select>
+		<?php } ?>
+		<input name="s" type="search" value="<?php echo esc_attr( get_query_var( 's' ) ); ?>" class="search-input" placeholder="<?php echo esc_attr( $block_booster_search_placeholder ); ?>" />
+		<button type="submit" class="search-button"><?php echo ! $block_booster_show_search_icon && $block_booster_button_text ? esc_html( $block_booster_button_text ) : '&nbsp;'; ?></button>
+	</div>
+	</form>
 </div>
 <style>
-  #<?php echo esc_attr($block_booster_additional_attributes['id']); ?> {
-    <?php if ($block_booster_height > 0) { ?>
-      <?php echo esc_html("height: {$block_booster_height}px;") ?>
-    <?php } ?>
-    <?php if ($block_booster_width > 0) { ?>
-      <?php echo esc_html("width: {$block_booster_width}px;") ?>
-    <?php } ?>
-  }
+	#<?php echo esc_attr( $block_booster_additional_attributes['id'] ); ?> {
+	<?php if ( $block_booster_height > 0 ) { ?>
+		<?php echo esc_html( "height: {$block_booster_height}px;" ); ?>
+	<?php } ?>
+	<?php if ( $block_booster_width > 0 ) { ?>
+		<?php echo esc_html( "width: {$block_booster_width}px;" ); ?>
+	<?php } ?>
+	}
 </style>
